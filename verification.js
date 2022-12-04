aaba7fb34e079e1f4368b83fbf142590a8b489b7
Ng Qian Zhe <s10243009@connect.np.edu.sg>
2022-12-04 21:10:37 +0800

    Commit

--- verification.js
+++ verification.js
@@ -7,23 +7,20 @@ function feedbackMessage() {
 // After pressing the login button, there will be alert message on the Login Page, stating your name, email and password
 // Redirecting you back to the homepage
 function loginMessage() {
-  let username = document.getElementById("name").value;
+  let username = document.getElementById("username").value;
   let emailAddr = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  alert("Thank you for filling in your details.")
-  alert("Name: " + username);
-  alert("Email: " + emailAddr);
-  alert("Password: " + password);
+  alert("Thank you for filling in your details.\nName: " + username + "\nEmail: " + emailAddr + "\nPassword: " + password);
 }
 
 // After pressing the add to cart button, there will be alert message on the shop page
 // Redirecting you back to the homepage
 function cartMessage() {
-  alert("Thank you for shopping at our shop.") 
+  alert("Thank you for shopping at our shop.");
 }
 
 // After filling in your contact details, and pressing the button, there will be alert message on the contact page
 // Redirecting you back to the homepage
 function contactMessage() {
-  alert("Thank you for keying in your details. We will update you on your email account.")
+  alert("Thank you for keying in your details. We will update you on your email account.");
 }
